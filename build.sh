4328f0c52abf4d14008a22c639aa5f15f1854569
Sumit Prajapati <162676130+sumit7444@users.noreply.github.com>
2026-04-15 16:29:07 +0530

    fixed path for requirements

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-#!/usr/bin/env bash
 set -o errexit
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r ecommerce/requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
